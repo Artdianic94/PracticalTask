74833e990ee9c4a8dae95d495fb6323b4c79b6bb
ArtDiaNic <Artdianic@gmail.com>
2023-02-20 16:45:20 +0300

    more options were added

--- src/test/java/driver/ChromeDriverManager.java
+++ src/test/java/driver/ChromeDriverManager.java
@@ -14,8 +14,12 @@ public class ChromeDriverManager {
     public ChromeDriverManager() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
         driver = new ChromeDriver(options);
     }
 
