eb6ea662ed36227fddfd89d3fd87edf7a9a26243
ArtDiaNic <Artdianic@gmail.com>
2023-02-22 14:55:36 +0300

    groovy with allure

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,46 +1,3 @@
-pipeline {
-    agent any
-
-    tools {
-        gradle "gradle jenkins"
-    }
-
-    stages {
-        stage('Build') {
-            steps {
-                // Get some code from a GitHub repository
-                git 'https://github.com/Artdianic94/PracticalTask.git'
-
-                // Run Gradle on a Unix agent.
-                 sh "gradle clean test"
-
-            }
-
-            post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
-                success {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                }
-            }
-        }
-            stage('reports') {
-                steps {
-                    script {
-                        allure([
-                                includeProperties: true,
-                                jdk              : '',
-                                properties       : [],
-                                reportBuildPolicy: 'ALWAYS',
-                                results          : [[path: 'build/allure-results']]
-                        ])
-                    }
-                }
-            }
-
-    }
-
-}
 task_branch = "${TEST_BRANCH_NAME}"
 def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
 currentBuild.displayName = "$branch_cutted"
@@ -63,9 +20,9 @@ node {
         }
 
         try {
-            getTestStages(["amazonAuthorizationTest","amazonSearchProductsTest","amazonAddProductTest","amazonProductInCartTest"])
+            getTestStages(["amazonAuthorizationTest", "amazonSearchProductsTest", "amazonAddProductTest", "amazonProductInCartTest"])
         } finally {
-            stage ("Allure") {
+            stage("Allure") {
                 generateAllure()
             }
         }
