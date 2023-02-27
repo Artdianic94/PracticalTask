3b351ae73b923871766a97162579432121b51687
ArtDiaNic <Artdianic@gmail.com>
2023-02-27 11:09:01 +0300

    getProject method was added

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,10 +1,10 @@
 task_branch = "${TEST_BRANCH_NAME}"
-//def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
-currentBuild.displayName = "$task_branch"
+def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
+currentBuild.displayName = "$branch_cutted"
 base_git_url = "https://github.com/Artdianic94/PracticalTask.git"
 
 node {
-    withEnv(["branch=${task_branch}", "base_url=${base_git_url}"]) {
+    withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
         stage("Checkout Branch") {
             if (!"$branch_cutted".contains("master")) {
                 try {
@@ -40,5 +40,15 @@ def generateAllure() {
             results          : [[path: 'build/allure-results']]
     ])
 }
+def getProject(String repo, String branch) {
+    cleanWs()
+    checkout scm: [
+            $class           : 'GitSCM', branches: [[name: branch]],
+            userRemoteConfigs: [[
+                                        url: repo
+                                ]]
+    ]
+}
+
 
 
