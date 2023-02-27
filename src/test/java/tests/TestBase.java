75280fae32c227602a208a213461580c5191bd15
ArtDiaNic <Artdianic@gmail.com>
2023-02-27 17:29:36 +0300

    Allure environment was added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,14 +1,22 @@
 package tests;
 
+import com.google.common.collect.ImmutableMap;
 import driver.ChromeDriverManager;
 import io.qameta.allure.Step;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.openqa.selenium.WebDriver;
 import pages.AmazonAuthorizationPage;
+import staticdata.WebUrls;
 import utilities.AfterEachExtension;
+
+import java.io.File;
+
+import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 
 public class TestBase {
     public WebDriver driver;
@@ -29,6 +37,16 @@ public class TestBase {
         amazonAuthorizationPage.makeLogin();
     }
 
+    @BeforeEach
+    void setAllureEnvironment() {
+        allureEnvironmentWriter(
+                ImmutableMap.<String, String>builder()
+                        .put("Browser", "Chrome")
+                        .put("Browser.Version", "110.0.5481.100")
+                        .put("URL", WebUrls.AMAZON_URL)
+                        .build(), System.getProperty("user.dir")
+                        + "/build/allure-results/");
+    }
 
     @AfterEach()
     @Step("Stop the application")
