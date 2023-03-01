4c47cb8a67250bd99b9b5135f7ea600026ecbefe
ArtDiaNic <Artdianic@gmail.com>
2023-03-01 11:20:37 +0300

    All the requested changes have been accepted.

--- src/test/java/pages/AmazonAuthorizationPage.java
+++ src/test/java/pages/AmazonAuthorizationPage.java
@@ -9,14 +9,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import staticdata.AmazonLoginCredentials;
-import staticdata.WebUrls;
-
 import java.time.Duration;
 
-
 public class AmazonAuthorizationPage extends BasePage {
-    WebDriverWait wait;
+    private final By YOUR_ACCOUNT = By.xpath("//a[contains(.,'Your Account')]");
     private final By SIGN_IN_ACCOUNT_LIST = By.id("nav-link-accountList");
     private final By SIGN_IN_BTN = By.id("nav-flyout-ya-signin");
     private final By EMAIL_INPUT = By.id("ap_email");
@@ -24,43 +20,44 @@ public class AmazonAuthorizationPage extends BasePage {
     private final By PASSWORD_INPUT = By.id("ap_password");
     private final By SIGN_IN_SUBMIT_BTN = By.id("signInSubmit");
     private final By USER_WELCOME_STRING = By.id("nav-link-accountList-nav-line-1");
+    private final By CAPTCHA_HEADER = By.xpath("//h4[@class='a-alert-heading']");
     private static final Logger LOGGER = LogManager.getLogger(AmazonAuthorizationPage.class.getName());
 
     public AmazonAuthorizationPage(WebDriver driver) {
         super(driver);
     }
 
-    @Step("Open main page " + WebUrls.AMAZON_URL)
-    public void openMainPage() {
-        LOGGER.info(String.format("Open website: %s", WebUrls.AMAZON_URL));
-        driver.get(WebUrls.AMAZON_URL);
+    @Step("Send login and password to the login form")
+    public void makeLogin(String email, String password) {
+        try {
+            driver.findElement(YOUR_ACCOUNT).click();
+            LOGGER.error("Another type of Amazon home page has been opened.");
+        } catch (NoSuchElementException ignored) {
+        }
+        moveOverAccountAndSignIn();
+        driver.findElement(EMAIL_INPUT).sendKeys(email);
+        driver.findElement(CONTINUE_BTN).click();
+        driver.findElement(PASSWORD_INPUT).sendKeys(password);
+        driver.findElement(SIGN_IN_SUBMIT_BTN).click();
     }
 
-    @Step("Send login and password to the login form")
-    public void makeLogin() {
-        Actions actions = new Actions(driver);
-        LOGGER.info("Move the cursor over 'Account & Lists' and wait for the tab to open.");
-        actions.moveToElement(driver.findElement(SIGN_IN_ACCOUNT_LIST));
+    @Step("Move the cursor over 'Account & Lists' and click 'Sign In'")
+    public void moveOverAccountAndSignIn() {
+        Actions actions = new Actions(driver).moveToElement(driver.findElement(SIGN_IN_ACCOUNT_LIST));
         actions.perform();
         LOGGER.info("Click 'Sign In' in the tab that opens.");
         driver.findElement(SIGN_IN_BTN).click();
-        LOGGER.error("Enter login and password.There may be additional confirmation requests.");
-        driver.findElement(EMAIL_INPUT).sendKeys(AmazonLoginCredentials.email);
-        driver.findElement(CONTINUE_BTN).click();
-        driver.findElement(PASSWORD_INPUT).sendKeys(AmazonLoginCredentials.password);
-        driver.findElement(SIGN_IN_SUBMIT_BTN).click();
     }
 
     @Step("Getting user greeting inscription")
     public String getSignInResult() {
         try {
-            LOGGER.info("Get user greeting inscription");
-//            wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-//            wait.until(ExpectedConditions.visibilityOfElementLocated(USER_WELCOME_STRING));
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(USER_WELCOME_STRING));
             return driver.findElement(USER_WELCOME_STRING).getText();
-        }catch (NoSuchElementException e){
-            LOGGER.error("You have to enter the characters manually");
-            return "Greeting inscription not found";
+        } catch (NoSuchElementException e) {
+            LOGGER.error("You should enter captcha manually");
+            return driver.findElement(CAPTCHA_HEADER).getText();
         }
     }
 }
